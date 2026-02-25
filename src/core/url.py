3ec63db5471340586227eb5dea0d0f85a5c4761a
Velez Joel <joelito.velez@gmail.com>
2026-02-25 10:42:50 -0300

    feat: CRUD - vistas y urls de models  cargo

---
+++ src/core/url.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cargo/lista/', views.CargoList.as_view(), name='cargo_list'),
+    path('cargo/detail/<', views.CargoDetail.as_view(), name='cargo_detail'),
+    path('cargo/create/', views.CargoCreate.as_view(), name='cargo_create'),
+    path('cargo/delete/', views.CargoDelete.as_view(), name='cargo_delete'),
+    path('cargo/update/', views.CargoUpdate.as_view(), name='cargo_update'),
+]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from . import models
+from . import forms
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CargoList(ListView):
+    model = models.Cargo
+    
+    def get_queryset(self):
+        consulta = self.request.GET.get('consulta')
+        if consulta:
+            cargo = models.Cargo.objects.filter(cargo__icontains=consulta)
+        else:
+            cargo = models.Cargo.objects.all()
+        return cargo
+    
+class CargoDetail(DetailView):
+    model = models.Cargo
+    
+class CargoCreate(CreateView):
+    model = models.Cargo
+    form_class = forms.CargoForm
+    success_url = reverse_lazy('core:cargo_list')
+    
+class CargoDelete(DeleteView):
+    model = models.Cargo
+    success_url = reverse_lazy('core:cargo_list')
+    
+class CargoUpdate(UpdateView):
+    model = models.Cargo
+    form_class = forms.CargoForm
+    success_url = reverse_lazy('core:cargo_list')
