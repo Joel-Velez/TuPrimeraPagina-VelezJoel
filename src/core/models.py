3f39fec73a90afbb95ccec7e2b29b71e34e7e966
Velez Joel <joelito.velez@gmail.com>
2026-03-05 14:28:47 -0300

    feat: se modifica modelo tarea - se modifica nombre core_tarea

--- src/core/models.py
+++ src/core/models.py
@@ -31,7 +31,7 @@ class Tarea(models.Model):
     nombre = models.CharField(max_length=250)
     descripcion = models.TextField()
     empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
+    # cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.nombre
