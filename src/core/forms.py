13d4b958d7628c2dbe8e460864573b22953e7c83
Velez Joel <joelito.velez@gmail.com>
2026-03-09 21:16:29 -0300

    feat: modificacion visual de forms.py con bootstrap e IA

--- src/core/forms.py
+++ src/core/forms.py
@@ -19,8 +19,23 @@ class TareaForm(forms.ModelForm):
         fields = '__all__'
         
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
