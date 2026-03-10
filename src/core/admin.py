c560d581b7c273dd579d29117e592def51635edb
Velez Joel <joelito.velez@gmail.com>
2026-03-10 08:24:54 -0300

    feat: egistramos modelos en admin.py

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cargo, Empleado, Tarea
+
+admin.site.register(Cargo)
+admin.site.register(Empleado)
+admin.site.register(Tarea)
